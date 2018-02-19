a0ac9d0690215cb7a980e8c731849a56b8c3c493
Бахарев Максим Евгеньевич <ip712s02@mwp08.csc.local>
2018-02-19 08:49:55 +0700

    Add chahmatnaia doska

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 int main ()
 {
-  printf ("Hello-World");
-  return 0;
+  printf ("R N B Q K B N R\n");
+  printf ("P P P P P P P P\n");
+  printf ("- - - - - - - -\n");
+  printf ("- - - - - - - -\n");
+  printf ("- - - - - - - -\n");
+  printf ("- - - - - - - -\n");
+  printf ("P P P P P P P P\n");
+  printf ("R N B Q K B N R\n");
+
+  getch ();
 }
